ad187494a08ba99c9a906a6f46f6b88f0ead5339
furka <furkan.sahinbusiness@gmail.com>
2023-12-24 23:52:58 +0300

    RoomController and FolioPageController coded

--- src/main/java/com/example/testfurkan/FolioPageController.java
+++ src/main/java/com/example/testfurkan/FolioPageController.java
@@ -1,13 +1,21 @@
 package com.example.testfurkan;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.ResourceBundle;
 
-public class FolioPageController {
+import BusinessLayer.BLL;
+import Entities.Folio;
+
+public class FolioPageController implements Initializable {
+    BLL bll = Main.bll;
     @FXML
     public ListView<String> productsListview;
     @FXML
@@ -27,9 +35,56 @@ public class FolioPageController {
     @FXML
     public Button payButton;
     @FXML
-    public TextField folioPayButton;
+    public TextField folioPayText;
+
+    public Folio folio;
+
+    public int roomID;
 
 
     public FolioPageController(int roomID){
+        this.roomID=roomID;
+        folio = bll.GetAllFolio(roomID);
     }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        totalCostLabel.setText(String.valueOf(folio.getBalance())+"$");
+        fillListViewWithProducts();
+        
+    }
+
+    public void payAllFolio()
+    {
+        totalCostLabel.setText(" ");
+        productsListview.setItems(null);
+    }
+    public void payFolio(){
+        double totalBalance = folio.getBalance();
+         
+        double newCost = totalBalance - Double.parseDouble(folioPayText.getText());
+        totalCostLabel.setText(String.valueOf(newCost)+"$");
+    }
+    public void addProductToFolio()
+    {
+        HashMap<String,Double> hashMap = folio.getProducts();
+        String addProductName = productNameField.getText();
+        Double addProductValue = (Double.parseDouble(priceField.getText())) * productCountSpinner.getValue();
+        hashMap.put(addProductName, addProductValue);
+        fillListViewWithProducts();
+    }
+    public void fillListViewWithProducts()
+    {
+        HashMap<String,Double> hashMap= folio.getProducts();
+        ObservableList<String> list = FXCollections.observableArrayList();
+        for (String key : hashMap.keySet()) {
+            Double value = hashMap.get(key);
+            String row = key + " ------------------> " + value.toString();
+            list.add(row);
+        }
+        productsListview.setItems(list);
+    }
+
 }
