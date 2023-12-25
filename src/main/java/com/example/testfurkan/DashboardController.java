c910eee818152b5efd8af8ed17623a28b203fca4
Alperen <furkanalperenarici@gmail.com>
2023-12-25 16:01:06 +0300

    sağ tık eklendi

--- src/main/java/com/example/testfurkan/DashboardController.java
+++ src/main/java/com/example/testfurkan/DashboardController.java
@@ -11,7 +11,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -51,9 +53,24 @@ public class DashboardController implements Initializable {
         stage.show();
     }
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        // Sağ tık menüsü oluştur
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem cleanItem = new MenuItem("Clean Room");
+
+        // MenuItem'lari ContextMenu'ya ekle
+        contextMenu.getItems().addAll(cleanItem);
+
+        cleanItem.setOnAction(e -> {
+            // "Cut" seçeneği için yapılacak işlemleri buraya ekleyin
+            Button button = (Button) e.getSource();
+            int bId = Integer.parseInt(button.getText());
+            List<Room> rooms = bll.GetAllRooms();
+            rooms.get(bId).setClean(true);
+            System.out.println(rooms.get(bId).isClean());
+        });
+
         dateNowLabel.setText(LocalDate.now().toString());
         int counter =0;
         for (Room room : bll.GetAllRooms()) {
@@ -75,27 +92,17 @@ public class DashboardController implements Initializable {
         List<Room> rooms = bll.GetAllRooms();
 
         for (int i = 0; i<10 ; i++){
+            Button button = (Button) myList.get(i);
+            button.getStyleClass().clear();
+            button.setContextMenu(contextMenu);
             if (!rooms.get(i).isEmpty()){
-                Button button = (Button) myList.get(i);
-                button.getStyleClass().clear();
                 button.getStyleClass().add("doluRoom");
             } else if (rooms.get(i).isClean()) {
-                Button button = (Button) myList.get(i);
-                button.getStyleClass().clear();
                 button.getStyleClass().add("activeRoom");
             }else  {
-                Button button = (Button) myList.get(i);
-                button.getStyleClass().clear();
                 button.getStyleClass().add("nonActiveRoom");
             }
         }
-
-        for (Room room : bll.GetAllRooms()) {
-            if(room.isEmpty()==true){
-                availableRoom++;
-            }
-        }
-        availableLabel.setText("Available Room : "+(String.valueOf(availableRoom)));
 
     }
     public void signoutButtonClick() throws IOException{
